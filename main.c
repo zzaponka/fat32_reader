728089a1ef996ce58f7948c11bdad06a6d946b01
Sergii Shydlovskyi <shydlovskyi.sergii@gmail.com>
2018-07-11 18:15:26 +0300

    4th stage
    Changes:
    - unicode support has been added to long filename entries

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <wchar.h>
 
 #include "fat32.h"
 
@@ -13,6 +14,10 @@ int bytes_per_sec;	//bytes per sector
 int read_sec(FILE *fd, int sec, int num, uint8_t *buf);
 
 char *int2bin(int a, char *buffer, int buf_size);
+
+void list_long_entry(uint8_t *buf, int size);
+
+void list_dir(uint8_t *buf);
 
 int main(int argc, char **argv)
 {
@@ -36,6 +41,9 @@ int main(int argc, char **argv)
 	t_dir_entry dir_entry;
 	t_long_dir_entry long_dir_entry;
 
+	printf("sizeof(t_dir_entry): %ld.\n", sizeof(t_dir_entry));
+	printf("sizeof(t_long_dir_entry): %ld.\n", sizeof(t_long_dir_entry));
+	printf("sizeof(wchar_t): %ld.\n", sizeof(wchar_t));
 	fd = fopen("./disk.img", "r");
 	if (!fd) {
 		perror("Failed to open file");
@@ -67,6 +75,7 @@ int main(int argc, char **argv)
 	// determine the count of sectors occupied by the root directory
 	printf("boot_entry.BPB_RootEntCnt: %d.\n", boot_entry.BPB_RootEntCnt);
 	printf("boot_entry.BPB_BytsPerSec: %d.\n", boot_entry.BPB_BytsPerSec);
+	// root_dir_sec is always 0 on FAT32 volumes
 	root_dir_sec = ((boot_entry.BPB_RootEntCnt * 32) + (boot_entry.BPB_BytsPerSec - 1)) / boot_entry.BPB_BytsPerSec;
 	printf("Number of sectors, occupied by the root directory: %ld.\n", root_dir_sec);
 
@@ -101,9 +110,10 @@ int main(int argc, char **argv)
 
 	//determine root directory sector number:
 	printf("Revelation: boot_entry.BPB_RootClus: %u!\n", boot_entry.BPB_RootClus);
+	// root_dir_sec_num is equal to first_data_sec
 	root_dir_sec_num = first_data_sec + (boot_entry.BPB_RootClus - 2) * sec_per_cluster;
 	printf("And root_dir_sec_num is %ld.\n", root_dir_sec_num);
-	buf = (unsigned char *)malloc(sizeof(unsigned char) * MAX_BUF);
+	buf = (uint8_t *)malloc(sizeof(uint8_t) * MAX_BUF);
 	if (!buf) {
 		perror("Memory allocation error");
 		exit(1);
@@ -122,7 +132,7 @@ int main(int argc, char **argv)
 		char i2b[9];
 		memcpy(&dir_entry, buf + i * sizeof(t_dir_entry), sizeof(t_dir_entry));
 		if (dir_entry.DIR_Name[0] == 0x00) {
-			// printf("%s@%d: #1: alas, we have missed!\n", __func__, __LINE__);
+			printf("%s@%d: #1: alas, we have missed!\n", __func__, __LINE__);
 			break;
 		}
 		printf("--------------------------------------------------\n");
@@ -145,6 +155,8 @@ int main(int argc, char **argv)
 			continue;
 		}
 		if (dir_entry.DIR_Attr == ATTR_LONG_NAME) {
+			int lde_last_num;
+			uint8_t *lde_buf;
 			// TODO:
 			// in order to find FAT entry for given cluster
 			// number N (which is contained inside DIR_FstClusHI
@@ -154,62 +166,22 @@ int main(int argc, char **argv)
 			// ThisFATSecNum = BPB_RsvdSecCnt + (FATOffset / BPB_BytsPerSec)
 			// ThisFATEntOffset FATOffset % BPB_BytsPerSec
 			// - pass these values to list_dir() recursively
-			int j;
 
 			// i++; // skipping
 			printf("ATTR_LONG_NAME bit is set, have to do something with that....\n");
 			memcpy(&long_dir_entry, buf + i * sizeof(t_long_dir_entry), sizeof(t_long_dir_entry));
-			printf("long_dir_entry.LDIR_Ord: 0x%01X.\n", long_dir_entry.LDIR_Ord);
-			printf("long_dir_entry.LDIR_Name1 (in hex): ");
-			for (j = 0; j < 10; j++) {
-				if (long_dir_entry.LDIR_Name1[j] == 0xFF)
-					break;
-				printf("%02X", long_dir_entry.LDIR_Name1[j]);
-			}
-			printf("\n");
-			printf("long_dir_entry.LDIR_Name1 (as chars): |");
-			for (j = 0; j < 10; j++) {
-				if (long_dir_entry.LDIR_Name1[j] == 0xFF)
-					break;
-				printf("%lc", long_dir_entry.LDIR_Name1[j]);
-			}
-			printf("|\n");
-			i2b[8] = '\0';
-			int2bin(long_dir_entry.LDIR_Attr, i2b, 8);
-			printf("long_dir_entry.LDIR_Attr: |%s|.\n", i2b);
-			printf("long_dir_entry.LDIR_Type: 0x%01X.\n", long_dir_entry.LDIR_Type);
-			printf("long_dir_entry.LDIR_Chksum: 0x%01X.\n", long_dir_entry.LDIR_Chksum);
-			// printf("long_dir_entry.LDIR_Name2: |%s|.\n", long_dir_entry.LDIR_Name2);
-			printf("long_dir_entry.LDIR_Name2 (in hex): ");
-			for (j = 0; j < 10; j++) {
-				if (long_dir_entry.LDIR_Name2[j] == 0xFF)
-					break;
-				printf("%02X", long_dir_entry.LDIR_Name2[j]);
-			}
-			printf("\n");
-			printf("long_dir_entry.LDIR_Name2 (as chars): |");
-			for (j = 0; j < 10; j++) {
-				if (long_dir_entry.LDIR_Name2[j] == 0xFF)
-					break;
-				printf("%lc", long_dir_entry.LDIR_Name2[j]);
-			}
-			printf("|\n");
-			printf("long_dir_entry.LDIR_FstClusLO: 0x%02X.\n", long_dir_entry.LDIR_FstClusLO);
-			// printf("long_dir_entry.LDIR_Name3: |%s|.\n", long_dir_entry.LDIR_Name3);
-			printf("long_dir_entry.LDIR_Name3 (in hex): ");
-			for (j = 0; j < 10; j++) {
-				if (long_dir_entry.LDIR_Name3[j] == 0xFF)
-					break;
-				printf("%02X", long_dir_entry.LDIR_Name3[j]);
-			}
-			printf("\n");
-			printf("long_dir_entry.LDIR_Name3 (as chars): |");
-			for (j = 0; j < 10; j++) {
-				if (long_dir_entry.LDIR_Name3[j] == 0xFF)
-					break;
-				printf("%lc", long_dir_entry.LDIR_Name3[j]);
-			}
-			printf("|\n");
+			// Zzz
+			printf("long_dir_entry.LDIR_Ord = %X.\n", long_dir_entry.LDIR_Ord);
+			lde_last_num = long_dir_entry.LDIR_Ord & ~LAST_LONG_ENTRY;
+			printf("lde_last_num = %d.\n", lde_last_num);
+			lde_buf = (uint8_t *)malloc(sizeof(uint8_t) * lde_last_num * 32);
+			memcpy(lde_buf, &long_dir_entry, sizeof(t_long_dir_entry) * lde_last_num);
+			printf("%s@%d: trying to print long filename entry, #0.\n", __func__, __LINE__);
+			list_long_entry(lde_buf, lde_last_num);
+			printf("%s@%d: trying to print long filename entry, #1.\n", __func__, __LINE__);
+		}
+		if (dir_entry.DIR_Attr == ATTR_DIRECTORY) {
+			printf("ATTR_DIRECTORY bit is set, have to pass the entry to directory listing fn().\n");
 		}
 		if (dir_entry.DIR_Attr & ATTR_VOLUME_ID) {
 			printf("ATTR_VOLUME_ID is set.\n");
@@ -222,9 +194,6 @@ int main(int argc, char **argv)
 		}
 		if (dir_entry.DIR_Attr & ATTR_SYSTEM) {
 			printf("ATTR_SYSTEM bit is set.\n");
-		}
-		if (dir_entry.DIR_Attr & ATTR_DIRECTORY) {
-			printf("ATTR_DIRECTORY bit is set.\n");
 		}
 		if (dir_entry.DIR_Attr & ATTR_ARCHIVE) {
 			printf("ATTR_ARCHIVE bit is set.\n");
@@ -278,4 +247,136 @@ char *int2bin(int a, char *buffer, int buf_size)
 	}
 
 	return buffer;
+}
+
+void list_long_entry(uint8_t *buf, int size)
+{
+	char *filename;
+	uint8_t flen;
+	int i;
+	int j;
+	int k;
+	t_long_dir_entry *lde;
+
+	flen = sizeof(char) * WCHAR_W * LDIR_ENT_FILENAME_LEN * size + 4;
+	printf("flen = %d.\n", flen);
+	filename = (char *)malloc(flen);
+	memset(filename, '\0', flen);
+
+	printf("Initially, filename in hex:\n");
+	for (k = 0; k < flen; k++)
+		printf("0x%02X ", filename[k]);
+	printf("\n");
+	j = 0;
+	for (i = 0; i < size; i++) {
+		lde = (t_long_dir_entry *)(buf + i * sizeof(t_long_dir_entry));
+		printf("sizeof(lde->LDIR_Name1): %ld.\n", sizeof(lde->LDIR_Name1));
+		printf("lde->LDIR_Name1 in hex:\n");
+		for (k = 0; k < sizeof(lde->LDIR_Name1); k++)
+			printf("0x%02X ", lde->LDIR_Name1[k]);
+		printf("\n");
+		k = 0;
+		while ((k < sizeof(lde->LDIR_Name1)) && (lde->LDIR_Name1[k] != 0xFF) && (lde->LDIR_Name1[k + 1] != 0xFF)) {
+			filename[j] = lde->LDIR_Name1[k];
+			filename[j + 1] = lde->LDIR_Name1[k + 1];
+			filename[j + 2] = 0x00;
+			filename[j + 3] = 0x00;
+			j += 4;
+			k += 2;
+		}
+		printf("sizeof(lde->LDIR_Name2): %ld.\n", sizeof(lde->LDIR_Name2));
+		printf("lde->LDIR_Name2 in hex:\n");
+		for (k = 0; k < sizeof(lde->LDIR_Name2); k++)
+			printf("0x%02X ", lde->LDIR_Name2[k]);
+		printf("\n");
+		k = 0;
+		while ((k < sizeof(lde->LDIR_Name2)) && (lde->LDIR_Name2[k] != 0xFF) && (lde->LDIR_Name2[k + 1] != 0xFF)) {
+			filename[j] = lde->LDIR_Name2[k];
+			filename[j + 1] = lde->LDIR_Name2[k + 1];
+			filename[j + 2] = 0x00;
+			filename[j + 3] = 0x00;
+			j += 4;
+			k += 2;
+		}
+		printf("sizeof(lde->LDIR_Name3): %ld.\n", sizeof(lde->LDIR_Name3));
+		printf("lde->LDIR_Name3 in hex:\n");
+		for (k = 0; k < sizeof(lde->LDIR_Name3); k++)
+			printf("0x%02X ", lde->LDIR_Name3[k]);
+		printf("\n");
+		k = 0;
+		while ((k < sizeof(lde->LDIR_Name3)) && (lde->LDIR_Name3[k] != 0xFF) && (lde->LDIR_Name3[k + 1] != 0xFF)) {
+			filename[j] = lde->LDIR_Name3[k];
+			filename[j + 1] = lde->LDIR_Name3[k + 1];
+			filename[j + 2] = 0x00;
+			filename[j + 3] = 0x00;
+			j += 4;
+			k += 2;
+		}
+	}
+	printf("After all, filename in hex:\n");
+	for (k = 0; k < flen; k++)
+		printf("0x%02X ", filename[k]);
+	printf("\n");
+	printf("After all, filename is: |%ls|.\n", (wchar_t *)filename);
+#if 0
+	// printf("Last long entry number: %d.\n", lde_last_num);
+	printf("lde->LDIR_Ord: 0x%01X.\n", lde->LDIR_Ord);
+	printf("lde->LDIR_Name1 (in hex): ");
+	for (j = 0; j < 10; j++) {
+		if (lde->LDIR_Name1[j] == 0xFF)
+			break;
+		printf("%02X", lde->LDIR_Name1[j]);
+	}
+	printf("\n");
+	printf("lde->LDIR_Name1 (as chars): |");
+	for (j = 0; j < 10; j++) {
+		if (lde->LDIR_Name1[j] == 0xFF)
+			break;
+		printf("%lc", lde->LDIR_Name1[j]);
+	}
+	printf("|\n");
+	i2b[8] = '\0';
+	int2bin(lde->LDIR_Attr, i2b, 8);
+	printf("lde->LDIR_Attr: |%s|.\n", i2b);
+	printf("lde->LDIR_Type: 0x%01X.\n", lde->LDIR_Type);
+	printf("lde->LDIR_Chksum: 0x%01X.\n", lde->LDIR_Chksum);
+	// printf("lde->LDIR_Name2: |%s|.\n", lde->LDIR_Name2);
+	printf("lde->LDIR_Name2 (in hex): ");
+	for (j = 0; j < 10; j++) {
+		if (lde->LDIR_Name2[j] == 0xFF)
+			break;
+		printf("%02X", lde->LDIR_Name2[j]);
+	}
+	printf("\n");
+	printf("lde->LDIR_Name2 (as chars): |");
+	for (j = 0; j < 10; j++) {
+		if (lde->LDIR_Name2[j] == 0xFF)
+			break;
+		printf("%lc", lde->LDIR_Name2[j]);
+	}
+	printf("|\n");
+	printf("lde->LDIR_FstClusLO: 0x%02X.\n", lde->LDIR_FstClusLO);
+	// printf("lde->LDIR_Name3: |%s|.\n", lde->LDIR_Name3);
+	printf("lde->LDIR_Name3 (in hex): ");
+	for (j = 0; j < 10; j++) {
+		if (lde->LDIR_Name3[j] == 0xFF)
+			break;
+		printf("%02X", lde->LDIR_Name3[j]);
+	}
+	printf("\n");
+	printf("lde->LDIR_Name3 (as chars): |");
+	for (j = 0; j < 10; j++) {
+		if (lde->LDIR_Name3[j] == 0xFF)
+			break;
+		printf("%lc", lde->LDIR_Name3[j]);
+	}
+	printf("|\n");
+#endif
+
+	return;
+}
+
+void list_dir(uint8_t *buf)
+{
+	// Zzz
 }
