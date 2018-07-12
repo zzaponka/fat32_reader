7edbd56147c42938d594a1a02e41bb5fb2a5b030
serg <serg@machost.local>
2018-07-12 11:35:30 +0300

    5th stage
    Changes:
    - unfinished
    
    Issues:
    - long filenames aren't always printed

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 // #include <fcntl.h>
 #include <assert.h>
+#include <locale.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +10,7 @@
 
 #include "fat32.h"
 
+t_boot_entry boot_entry;
 int bytes_per_sec;	//bytes per sector
 
 int read_sec(FILE *fd, int sec, int num, uint8_t *buf);
@@ -17,14 +19,14 @@ char *int2bin(int a, char *buffer, int buf_size);
 
 void list_long_entry(uint8_t *buf, int size);
 
-void list_dir(uint8_t *buf);
+// void list_dir(uint8_t *buf);
+void list_dir(t_dir_entry *buf);
 
 int main(int argc, char **argv)
 {
 	FILE *fd;
 	int num;
 	unsigned char *buf;
-	t_boot_entry boot_entry;
 	int sec_per_cluster;	//sectors per cluster
 	long root_dir_sec;	//count of sectors occupied by the root dir
 	long FATSz;		//FAT size
@@ -33,17 +35,15 @@ int main(int argc, char **argv)
 	// long tot_sec;		//total count of sectors on the volume
 	// long num_data_sec;	//count of sectors in the volume's data region
 	// long num_clusters;	//count of clusters in the volume
-	int i;
+	// int i;
 	// int fat_offset;
 	// int this_fat_sec_num;
 	// int this_fat_ent_offset;
 	// uint32_t fat32_cluster_entry_val;
-	t_dir_entry dir_entry;
-	t_long_dir_entry long_dir_entry;
+	// t_dir_entry dir_entry;
+	// t_long_dir_entry long_dir_entry;
 
-	printf("sizeof(t_dir_entry): %ld.\n", sizeof(t_dir_entry));
-	printf("sizeof(t_long_dir_entry): %ld.\n", sizeof(t_long_dir_entry));
-	printf("sizeof(wchar_t): %ld.\n", sizeof(wchar_t));
+	setlocale(LC_ALL, "C.UTF-8");
 	fd = fopen("./disk.img", "r");
 	if (!fd) {
 		perror("Failed to open file");
@@ -124,82 +124,9 @@ int main(int argc, char **argv)
 	assert((FATSz * boot_entry.BPB_NumFATs * boot_entry.BPB_BytsPerSec) < MAX_BUF);
 	// read_sec(fd, root_dir_sec_num, sec_per_cluster, buf);
 	read_sec(fd, root_dir_sec_num, FATSz * boot_entry.BPB_NumFATs * boot_entry.BPB_BytsPerSec, buf);
-	printf("%s@%d: #0\n", __func__, __LINE__);
 
 	//reading clusters sequentally
-	for (i = 0; i < (FATSz * boot_entry.BPB_NumFATs * boot_entry.BPB_BytsPerSec) / 32; i++) {
-		uint8_t *dir_entry_p;
-		char i2b[9];
-		memcpy(&dir_entry, buf + i * sizeof(t_dir_entry), sizeof(t_dir_entry));
-		if (dir_entry.DIR_Name[0] == 0x00) {
-			printf("%s@%d: #1: alas, we have missed!\n", __func__, __LINE__);
-			break;
-		}
-		printf("--------------------------------------------------\n");
-		// printf("> Entry #%d, hex: 0x%08X.\n", i, (uint32_t )dir_entry);
-		printf("> Entry #%d.\n", i);
-		dir_entry_p = buf + i * sizeof(t_dir_entry);
-		printf("As hex: 0x%08X.\n", (unsigned int )*dir_entry_p);
-		printf("dir_entry.DIR_FileSize: %u.\n", dir_entry.DIR_FileSize);
-		printf("dir_entry.DIR_FstClusHI: %u.\n", dir_entry.DIR_FstClusHI);
-		printf("dir_entry.DIR_FstClusLO: %u.\n", dir_entry.DIR_FstClusLO);
-		uint8_t *p;
-		p = (uint8_t *)&dir_entry.DIR_WrtDate;
-		printf("dir_entry.DIR_WrtDate: 0x%02x, 0x%02x.\n", *p, *(p + 1));
-		printf("dir_entry.DIR_WrtDate: 0x%04x.\n", dir_entry.DIR_WrtDate);
-		i2b[8] = '\0';
-		int2bin(dir_entry.DIR_Attr, i2b, 8);
-		printf("dir_entry.DIR_Attr: |%s|.\n", i2b);
-		if (dir_entry.DIR_Attr == ATTR_VOLUME_ID) {
-			printf("Only ATTR_VOLUME_ID is set, skipping...\n");
-			continue;
-		}
-		if (dir_entry.DIR_Attr == ATTR_LONG_NAME) {
-			int lde_last_num;
-			uint8_t *lde_buf;
-			// TODO:
-			// in order to find FAT entry for given cluster
-			// number N (which is contained inside DIR_FstClusHI
-			// and DIR_FstClusLO) we need:
-			// - calculate:
-			// FATOffset = N * 4
-			// ThisFATSecNum = BPB_RsvdSecCnt + (FATOffset / BPB_BytsPerSec)
-			// ThisFATEntOffset FATOffset % BPB_BytsPerSec
-			// - pass these values to list_dir() recursively
-
-			// i++; // skipping
-			printf("ATTR_LONG_NAME bit is set, have to do something with that....\n");
-			memcpy(&long_dir_entry, buf + i * sizeof(t_long_dir_entry), sizeof(t_long_dir_entry));
-			// Zzz
-			printf("long_dir_entry.LDIR_Ord = %X.\n", long_dir_entry.LDIR_Ord);
-			lde_last_num = long_dir_entry.LDIR_Ord & ~LAST_LONG_ENTRY;
-			printf("lde_last_num = %d.\n", lde_last_num);
-			lde_buf = (uint8_t *)malloc(sizeof(uint8_t) * lde_last_num * 32);
-			memcpy(lde_buf, &long_dir_entry, sizeof(t_long_dir_entry) * lde_last_num);
-			printf("%s@%d: trying to print long filename entry, #0.\n", __func__, __LINE__);
-			list_long_entry(lde_buf, lde_last_num);
-			printf("%s@%d: trying to print long filename entry, #1.\n", __func__, __LINE__);
-		}
-		if (dir_entry.DIR_Attr == ATTR_DIRECTORY) {
-			printf("ATTR_DIRECTORY bit is set, have to pass the entry to directory listing fn().\n");
-		}
-		if (dir_entry.DIR_Attr & ATTR_VOLUME_ID) {
-			printf("ATTR_VOLUME_ID is set.\n");
-		}
-		if (dir_entry.DIR_Attr & ATTR_READ_ONLY) {
-			printf("ATTR_READ_ONLY bit is set.\n");
-		}
-		if (dir_entry.DIR_Attr & ATTR_HIDDEN) {
-			printf("ATTR_HIDDEN bit is set.\n");
-		}
-		if (dir_entry.DIR_Attr & ATTR_SYSTEM) {
-			printf("ATTR_SYSTEM bit is set.\n");
-		}
-		if (dir_entry.DIR_Attr & ATTR_ARCHIVE) {
-			printf("ATTR_ARCHIVE bit is set.\n");
-		}
-		printf("%d: filename: %s, first char: %c.\n", i, dir_entry.DIR_Name, dir_entry.DIR_Name[0]);
-	}
+	list_dir((t_dir_entry *)buf);
 
 	fclose(fd);
 
@@ -211,27 +138,16 @@ int read_sec(FILE *fd, int sec, int num, uint8_t *buf)
 	int pos;
 	int len;
 
-	printf("%s@%d: #0\n", __func__, __LINE__);
 	pos = fseek(fd, sec * bytes_per_sec , SEEK_SET);
-	printf("%s@%d: #1\n", __func__, __LINE__);
 	if (pos) {
 		perror("Can't find position inside the file");
 		exit(1);
 	}
-	printf("%s@%d: #2\n", __func__, __LINE__);
-#if 0
-	if (num * bytes_per_sec > MAX_BUF) {
-		perror("Buffer size too small");
-		exit(1);
-	}
-#endif
 	len = fread(buf, 1, num, fd);
-	printf("%s@%d: #3\n", __func__, __LINE__);
 	if (len != num) {
 		perror("Error in reading sector\n");
 		exit(1);
 	}
-	printf("%s@%d: #4\n", __func__, __LINE__);
 
 	return len;
 }
@@ -259,7 +175,7 @@ void list_long_entry(uint8_t *buf, int size)
 	t_long_dir_entry *lde;
 
 	flen = sizeof(char) * WCHAR_W * LDIR_ENT_FILENAME_LEN * size + 4;
-	printf("flen = %d.\n", flen);
+	printf("%s@%d: flen = %d.\n", __func__, __LINE__, flen);
 	filename = (char *)malloc(flen);
 	memset(filename, '\0', flen);
 
@@ -315,7 +231,7 @@ void list_long_entry(uint8_t *buf, int size)
 	}
 	printf("After all, filename in hex:\n");
 	for (k = 0; k < flen; k++)
-		printf("0x%02X ", filename[k]);
+		printf("%d|0x%02X ", k, filename[k]);
 	printf("\n");
 	printf("After all, filename is: |%ls|.\n", (wchar_t *)filename);
 #if 0
@@ -376,7 +292,147 @@ void list_long_entry(uint8_t *buf, int size)
 	return;
 }
 
-void list_dir(uint8_t *buf)
+// void list_dir(uint8_t *buf)
+void list_dir(t_dir_entry *buf)
 {
-	// Zzz
+	int i;
+
+	i = 0;
+	while (1) {
+		// uint8_t *dir_entry_p;
+
+		// memcpy(&dir_entry, buf + i * sizeof(t_dir_entry), sizeof(t_dir_entry));
+		printf("==================================================\n");
+		printf("Iteration #%d.\n", i);
+		printf("buf->DIR_Attr: %02x.\n", buf->DIR_Attr);
+		if (buf->DIR_Attr == ATTR_VOLUME_ID) {
+			printf("Only ATTR_VOLUME_ID is set, skipping...\n");
+			// continue;
+		}
+		if (buf->DIR_Name[0] == 0x00) {
+			printf("%s@%d: #1: alas, we have missed!\n", __func__, __LINE__);
+			break;
+		};
+		if (buf->DIR_Attr == ATTR_LONG_NAME) {
+			int lde_last_num;
+			// uint8_t *lde_buf;
+			t_long_dir_entry *lde_p;
+			// TODO:
+			// in order to find FAT entry for given cluster
+			// number N (which is contained inside DIR_FstClusHI
+			// and DIR_FstClusLO) we need:
+			// - calculate:
+			// FATOffset = N * 4
+			// ThisFATSecNum = BPB_RsvdSecCnt + (FATOffset / BPB_BytsPerSec)
+			// ThisFATEntOffset FATOffset % BPB_BytsPerSec
+			// - pass these values to list_dir() recursively
+
+			// i++; // skipping
+			printf("ATTR_LONG_NAME bit is set, have to do something with that....\n");
+			// memcpy(&long_dir_entry, buf + i * sizeof(t_long_dir_entry), sizeof(t_long_dir_entry));
+			// Zzz
+			lde_p = (t_long_dir_entry *)buf;
+			printf("buf->LDIR_Ord = %X.\n", lde_p->LDIR_Ord);
+			lde_last_num = lde_p->LDIR_Ord & ~LAST_LONG_ENTRY;
+			printf("lde_last_num = %d.\n", lde_last_num);
+			// lde_buf = (uint8_t *)malloc(sizeof(uint8_t) * lde_last_num * 32);
+			// memcpy(lde_buf, &long_dir_entry, sizeof(t_long_dir_entry) * lde_last_num);
+			printf("%s@%d: trying to print long filename entry, #0.\n", __func__, __LINE__);
+			list_long_entry((uint8_t *)lde_p, lde_last_num);
+			printf("%s@%d: trying to print long filename entry, #1.\n", __func__, __LINE__);
+		}
+		if (buf->DIR_Attr == ATTR_DIRECTORY) {
+			printf("ATTR_DIRECTORY bit is set, have to pass the entry to directory listing fn().\n");
+			uint8_t *sec_val;
+			uint32_t fat_offset;
+			uint32_t this_fat_sec_num;
+			uint32_t this_fat_end_offset;
+
+			sec_val = (uint8_t *)malloc(sizeof(uint32_t));
+			memcpy(sec_val, &buf->DIR_FstClusLO, sizeof(uint16_t));
+			memcpy(sec_val + 2, &buf->DIR_FstClusHI, sizeof(uint16_t));
+			fat_offset = (*sec_val) * 4;
+			printf("Finally, fat_offset: %d.\n", fat_offset);
+			this_fat_sec_num = boot_entry.BPB_RsvdSecCnt + (fat_offset / boot_entry.BPB_BytsPerSec);
+			this_fat_end_offset = fat_offset % boot_entry.BPB_BytsPerSec;
+			printf("Finally, this_fat_sec_num: %d.\n", this_fat_sec_num);
+			printf("Finally, this_fat_end_offset: %d.\n", this_fat_end_offset);
+
+			list_dir(buf + this_fat_end_offset);
+		}
+		printf("%s@%d: incrementing i...\n", __func__, __LINE__);
+		i++;
+		printf("%s@%d: shifting buf...\n", __func__, __LINE__);
+		// buf = buf + sizeof(t_dir_entry);
+		buf++;
+		// Zzz
+	}
+#if 0
+	for (i = 0; i < (FATSz * boot_entry.BPB_NumFATs * boot_entry.BPB_BytsPerSec) / 32; i++) {
+		char i2b[9];
+		memcpy(&dir_entry, buf + i * sizeof(t_dir_entry), sizeof(t_dir_entry));
+		if (dir_entry.DIR_Name[0] == 0x00) {
+			printf("%s@%d: #1: alas, we have missed!\n", __func__, __LINE__);
+			break;
+		}
+		printf("--------------------------------------------------\n");
+		// printf("> Entry #%d, hex: 0x%08X.\n", i, (uint32_t )dir_entry);
+		printf("> Entry #%d.\n", i);
+		dir_entry_p = buf + i * sizeof(t_dir_entry);
+		printf("As hex: 0x%08X.\n", (unsigned int )*dir_entry_p);
+		printf("dir_entry.DIR_FileSize: %u.\n", dir_entry.DIR_FileSize);
+		printf("dir_entry.DIR_FstClusHI: %u.\n", dir_entry.DIR_FstClusHI);
+		printf("dir_entry.DIR_FstClusLO: %u.\n", dir_entry.DIR_FstClusLO);
+		uint8_t *p;
+		p = (uint8_t *)&dir_entry.DIR_WrtDate;
+		printf("dir_entry.DIR_WrtDate: 0x%02x, 0x%02x.\n", *p, *(p + 1));
+		printf("dir_entry.DIR_WrtDate: 0x%04x.\n", dir_entry.DIR_WrtDate);
+		i2b[8] = '\0';
+		int2bin(dir_entry.DIR_Attr, i2b, 8);
+		printf("dir_entry.DIR_Attr: |%s|.\n", i2b);
+		if (dir_entry.DIR_Attr == ATTR_LONG_NAME) {
+			int lde_last_num;
+			uint8_t *lde_buf;
+			// TODO:
+			// in order to find FAT entry for given cluster
+			// number N (which is contained inside DIR_FstClusHI
+			// and DIR_FstClusLO) we need:
+			// - calculate:
+			// FATOffset = N * 4
+			// ThisFATSecNum = BPB_RsvdSecCnt + (FATOffset / BPB_BytsPerSec)
+			// ThisFATEntOffset FATOffset % BPB_BytsPerSec
+			// - pass these values to list_dir() recursively
+
+			// i++; // skipping
+			printf("ATTR_LONG_NAME bit is set, have to do something with that....\n");
+			memcpy(&long_dir_entry, buf + i * sizeof(t_long_dir_entry), sizeof(t_long_dir_entry));
+			// Zzz
+			printf("long_dir_entry.LDIR_Ord = %X.\n", long_dir_entry.LDIR_Ord);
+			lde_last_num = long_dir_entry.LDIR_Ord & ~LAST_LONG_ENTRY;
+			printf("lde_last_num = %d.\n", lde_last_num);
+			lde_buf = (uint8_t *)malloc(sizeof(uint8_t) * lde_last_num * 32);
+			memcpy(lde_buf, &long_dir_entry, sizeof(t_long_dir_entry) * lde_last_num);
+			printf("%s@%d: trying to print long filename entry, #0.\n", __func__, __LINE__);
+			list_long_entry(lde_buf, lde_last_num);
+			printf("%s@%d: trying to print long filename entry, #1.\n", __func__, __LINE__);
+		}
+		if (dir_entry.DIR_Attr & ATTR_VOLUME_ID) {
+			printf("ATTR_VOLUME_ID is set.\n");
+		}
+		if (dir_entry.DIR_Attr & ATTR_READ_ONLY) {
+			printf("ATTR_READ_ONLY bit is set.\n");
+		}
+		if (dir_entry.DIR_Attr & ATTR_HIDDEN) {
+			printf("ATTR_HIDDEN bit is set.\n");
+		}
+		if (dir_entry.DIR_Attr & ATTR_SYSTEM) {
+			printf("ATTR_SYSTEM bit is set.\n");
+		}
+		if (dir_entry.DIR_Attr & ATTR_ARCHIVE) {
+			printf("ATTR_ARCHIVE bit is set.\n");
+		}
+		printf("%d: filename: %s, first char: %c.\n", i, dir_entry.DIR_Name, dir_entry.DIR_Name[0]);
+	}
+#endif
+	printf("%s@%d: called.\n", __func__, __LINE__);
 }
